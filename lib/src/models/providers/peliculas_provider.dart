c0979978059f385db9c13dce7103759467c226cf
Edri Angulo <edri.alan.94@gmail.com>
2021-06-03 20:44:14 -0500

    1-Se agrego pagieview para ver peliculas populares

--- lib/src/models/providers/peliculas_provider.dart
+++ lib/src/models/providers/peliculas_provider.dart
@@ -8,23 +8,31 @@ class PeliculasProvider{
   String _url = 'api.themoviedb.org';
   String _language = "es-MX";
 
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+    return peliculas.items;
+  }
+
   Future<List<Pelicula>> getEnCines() async{
     final url = Uri.https(_url, '3/movie/now_playing', {
       'api_key' : _apiKey,
       'language': _language
     });
 
-    final resp = await http.get(url);
-    final decodedData = json.decode(resp.body);
-
-    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
-
-    return peliculas.items;
-
+    return await _procesarRespuesta(url);
   }
 
- 
 
+   Future<List<Pelicula>> getPopulares() async{
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key' : _apiKey,
+      'language': _language
+    });
 
-
+    return await _procesarRespuesta(url);
+   }
+   
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:peliculas/src/models/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   //const HomePage({Key key}) : super(key: key);
@@ -23,6 +24,7 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             _swiperTarjetas(),
             _footer(context)
@@ -57,11 +59,27 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _footer(BuildContext context) {
+    peliculasProvider.getPopulares();
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('populares', style: Theme.of(context).textTheme.subtitle1)
+          Container(padding: EdgeInsets.only(left: 20.0), child: Text('populares', style: Theme.of(context).textTheme.subtitle1)),
+          SizedBox(height: 5.0),
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+              snapshot.data?.forEach((p) => print(p.title));
+              if(snapshot.hasData){
+                return MovieHorizontal(peliculas: snapshot.data);
+              }
+              else 
+              {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ],),
     );
   }
